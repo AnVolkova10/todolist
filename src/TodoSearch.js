428b0e5395192abb2efbcae1704808bf570df3c9
AnVolkova10 <curzi.angela@gmail.com>
2022-01-04 10:46:48 -0300

    adding some events

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,17 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+
+    const onSearchValueChange  = (e) => {
+        console.log(e.target.value);
+    };
+
     return (
-        <input className='TodoSearch' placeholder="Genosha" />
+        <input 
+            className='TodoSearch' 
+            placeholder="Genosha"
+            onChange={onSearchValueChange} 
+        />
     )
 }
 
