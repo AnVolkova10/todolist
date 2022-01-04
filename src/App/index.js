3449c83edc72428ec3d84a9a9f512f9bc66215de
AnVolkova10 <curzi.angela@gmail.com>
2022-01-04 13:50:24 -0300

    re-organized files & folders

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { CreateTodoButton } from './CreateTodoButton';
-import { TodoItem } from './TodoItem';
-import { TodoList } from './TodoList';
-import './App.css';
+import { AppUI } from './AppUI';
+
+
 
 const defaultTodos = [
   {text: 'Quedar en Genosha', completed: false},
@@ -49,38 +46,16 @@ function App() {
     setTodos(newTodos);
   }
 
-
-
-  
-
   return (
-    <React.Fragment>
-      <TodoCounter 
-        total={totalTodos}
-        completed={completedTodos}
-      />
-
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-      
-      <TodoList > 
-        {searchedTodos.map(todo => (
-          <TodoItem 
-            key={todo.text} 
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={()=> deleteTodo(todo.text)}
-            
-            />
-        ))}
-       </TodoList >
-
-      <CreateTodoButton/>
-
-    </React.Fragment>
+    <AppUI
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
