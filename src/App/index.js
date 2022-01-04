f4971dc46dfda49a245168550adeeb8d462baa6f
AnVolkova10 <curzi.angela@gmail.com>
2022-01-04 14:57:00 -0300

    added localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -3,16 +3,28 @@ import { AppUI } from './AppUI';
 
 
 
-const defaultTodos = [
-  {text: 'Quedar en Genosha', completed: false},
-  {text: 'Hacer ejercicio', completed: true},
-  {text: 'Terminar de leer La Inmortalidad', completed: false}
-];
-
 function App() {
 
+  //Local Storage
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+  
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
+
   //Props
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -35,7 +47,7 @@ function App() {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   //Delete Todo
@@ -43,7 +55,7 @@ function App() {
     const todoIndex = todos.findIndex(todo =>todo.text=== text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
